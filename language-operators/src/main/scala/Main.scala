f93045d562a1644f498d52a303fcfa3acda66acf
Tautvidas Sipavicius <t.9b5as@gmail.com>
2014-02-23 14:17:31 +0200

    Test for integer overflows

--- language-operators/src/main/scala/Main.scala
+++ language-operators/src/main/scala/Main.scala
@@ -4,6 +4,7 @@ object Main extends App {
     if (args.length != 2) {
         println("Usage: sbt \"run <function> <argument>\"")
     } else {
+        println(args(0))
         val f = Function.parse(args(0))
         f match {
             case Some(func : (Int => Option[Int])) => println(func(args(1).toInt))
--- language-operators/src/test/scala/TestTest.scala
+++ language-operators/src/test/scala/TestTest.scala
@@ -27,6 +27,15 @@ class TestFunction extends FunSuite {
         }
     }
 
+    test("negative integer overflows are handled properly") {
+        expect(Some(2147483549)) {
+            Function.parse("(x: Int) => x % 65535 - 2147483647") match {
+                case Some(f: (Int => Some[Int])) => f(-100)
+                case _ => None
+            }
+        }
+    }
+
     test("malformed function (missing `=>`) fails") {
         expect(None) { Function.parse("(x: Int) = 1 - x % x") }
     }
