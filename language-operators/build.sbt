d2e9ff2f66df57ae2b07eeb534e6f72201a29c71
Tautvidas Sipavicius <t.9b5as@gmail.com>
2014-03-29 13:26:23 +0200

    Added scalacheck and an example test, refactored previous tests

--- language-operators/build.sbt
+++ language-operators/build.sbt
@@ -1,7 +1,9 @@
 name := "language-operators"
 
-version := "0.1"
+version := "0.2"
 
 scalaVersion := "2.10.3"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.0" % "test"
+
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.3" % "test"
--- language-operators/src/test/scala/TestTest.scala
+++ language-operators/src/test/scala/TestTest.scala
@@ -1,38 +1,33 @@
 package languageops
 
-import org.scalatest.FunSuite
+import org.scalatest._
+import org.scalatest.prop.Checkers
+import org.scalacheck._
+import org.scalacheck.Prop.{forAll, BooleanOperators}
 
-class TestFunction extends FunSuite {
+class TestFunction extends FunSuite with Checkers {
     test("correctly calculates function output") {
         expect(Some(5)) {
-            Function.parse("(x: Int) => 5 - x % x") match {
-                case Some(f: (Int => Some[Int])) => f(17)
-                case _ => None
-            }
+            val func = Function.parse("(x: Int) => 5 - x % x")
+            func.flatMap((f: (Int => Option[Int])) => f(17))
         }
         expect(Some(-6)) {
-            Function.parse("(x: Int) => 1 - x % 10") match {
-                case Some(f: (Int => Some[Int])) => f(17)
-                case _ => None
-            }
+            val func = Function.parse("(x: Int) => 1 - x % 10")
+            func.flatMap((f: (Int => Option[Int])) => f(17))
         }
     }
 
     test("division by zero returns false") {
         expect(None) {
-            Function.parse("(x: Int) => 1 - x % x") match {
-                case Some(f: (Int => Some[Int])) => f(0)
-                case _ => None
-            }
+            val func = Function.parse("(x: Int) => 1 - x % x")
+            func.flatMap((f: (Int => Option[Int])) => f(0))
         }
     }
 
     test("negative integer overflows are handled properly") {
         expect(Some(2147483549)) {
-            Function.parse("(x: Int) => x % 65535 - 2147483647") match {
-                case Some(f: (Int => Some[Int])) => f(-100)
-                case _ => None
-            }
+            val func = Function.parse("(x: Int) => x % 65535 - 2147483647")
+            func.flatMap((f: (Int => Option[Int])) => f(-100))
         }
     }
 
@@ -42,20 +37,27 @@ class TestFunction extends FunSuite {
 
     test("does not accept arguments from (-inf, -100) U (100, inf)") {
         expect(None) {
-            Function.parse("(x: Int) => 1 - x % 10") match {
-                case Some(f: (Int => Some[Int])) => f(-1000)
-                case _ => None
-            }
+            val func = Function.parse("(x: Int) => 1 - x % 10")
+            func.flatMap((f: (Int => Option[Int])) => f(-1000))
         }
         expect(None) {
-            Function.parse("(x: Int) => 1 - x % 10") match {
-                case Some(f: (Int => Some[Int])) => f(1000)
-                case _ => None
-            }
+            val func = Function.parse("(x: Int) => 1 - x % 10")
+            func.flatMap((f: (Int => Option[Int])) => f(1000))
         }
     }
 
     test("verifies that no other variable names are used") {
         expect(None) { Function.parse("(x: Int) => x - y % 10") }
+    }
+
+    test("'var % const - const' class function computes properly") {
+        val argumentNames = Gen.identifier.suchThat(_ != "")
+        val argumentValues = Gen.choose(-100, 100)
+        check(Prop.forAll(argumentNames, argumentValues) {
+            (arg: String, param: Int) => {
+                val func = Function.parse("(" + arg + ": Int) => " + arg + " % 101 - 0")
+                (func.flatMap((f: (Int => Option[Int])) => f(param))) == Some(param)
+            }
+        })
     }
 }
