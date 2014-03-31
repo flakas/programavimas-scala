1d3c1f00db10110ae1a758f5b88d6839a5d1f5a2
Tautvidas Sipavicius <t.9b5as@gmail.com>
2014-03-31 17:55:39 +0300

    Refactoring

--- language-operators/src/test/scala/FunctionSpec.scala
+++ language-operators/src/test/scala/FunctionSpec.scala
@@ -8,28 +8,28 @@ import org.scalacheck.Prop.{forAll, BooleanOperators}
 class FunctionSpec extends FlatSpec with Matchers with Checkers {
 
     "A Function" should "correctly calculate output with valid input" in {
-        val func = Function.parse("(x: Int) => 5 - x % x")
-        func.flatMap((f: (Int => Option[Int])) => f(17)) should be (Some(5))
+        val f = Function.parse("(x: Int) => 5 - x % x").get
+        f(17) should be (Some(5))
     }
 
     it should "return false on division by zero" in {
-        val f = Function.parse("(x: Int) => 1 - x % x")
-        f.flatMap((f: (Int => Option[Int])) => f(0)) should be (None)
+        val f = Function.parse("(x: Int) => 1 - x % x").get
+        f(0) shouldBe empty
     }
 
     it should "handle integer overflows properly" in {
-        val f = Function.parse("(x: Int) => x % 65535 - 2147483647")
-        f.flatMap((f: (Int => Option[Int])) => f(-100)) should be (Some(2147483549))
+        val f = Function.parse("(x: Int) => x % 65535 - 2147483647").get
+        f(-100) should be (Some(2147483549))
     }
 
     it should "return none if function is malformed" in {
-        Function.parse("(x: Int) = 1 - x % x") should be (None)
+        Function.parse("(x: Int) = 1 - x % x") shouldBe empty
     }
 
     it should "not accept arguments from (-inf, -100) U (100, inf)" in {
-        val f = Function.parse("(x: Int) => 1 - x % 10")
-        f.flatMap((f: (Int => Option[Int])) => f(-1000)) should be (None)
-        f.flatMap((f: (Int => Option[Int])) => f(1000)) should be (None)
+        val f = Function.parse("(x: Int) => 1 - x % 10").get
+        f(-1000) shouldBe empty
+        f(1000) shouldBe empty
     }
 
     it should "not accept undefined variables" in {
@@ -37,8 +37,8 @@ class FunctionSpec extends FlatSpec with Matchers with Checkers {
     }
 
     it should "respect arithmetic function order" in {
-        Function.parse("(x: Int) => 4 - x % 2")
-            .flatMap((f: (Int => Option[Int])) => f(5)) should be (Some(3))
+        val f = Function.parse("(x: Int) => 4 - x % 2").get
+        f(5) should be (Some(3))
     }
 
     "Functions of all valid classes" should "compute properly with valid inputs" in {
@@ -67,8 +67,8 @@ class FunctionSpec extends FlatSpec with Matchers with Checkers {
                     .replaceAll("%const1%", c1.toString)
                     .replaceAll("%const2%", c2.toString)
                 val testFunction = functions(functionTemplate)
-                val func = Function.parse(functionText)
-                func.flatMap((f: (Int => Option[Int])) => f(param)) == Some(testFunction(param, c1, c2))
+                val f = Function.parse(functionText).get
+                f(param) == Some(testFunction(param, c1, c2))
             }
         })
     }
